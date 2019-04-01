a5f97351fc90659364f735c5f3c423aac1504614
seothemes <seothemeswp@gmail.com>
2019-04-01 18:49:45 +0800

    Initial commit

---
+++ functions.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Genesis Customizer
+ *
+ * This file adds functions to the Genesis Customizer theme.
+ *
+ * @package GenesisCustomizer\Theme
+ * @author  SEO Themes
+ * @license GPL-3.0-or-later
+ * @link    https://www.genesiscustomizer.com/
+ */
+
+// Starts the engine (do not remove).
+require_once get_stylesheet_directory() . '/vendor/autoload.php';
